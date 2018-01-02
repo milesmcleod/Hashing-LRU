d4fa834c100d98741073b569e107cf6022f33b2a
Miles McLeod <milesmcleod123@gmail.com>
2018-01-01 23:21:46 -0300

    Complete hashing section

--- firstname_lastname 5/lib/p02_hashing.rb
+++ firstname_lastname 5/lib/p02_hashing.rb
@@ -4,11 +4,23 @@ end
 
 class Array
   def hash
+    hash = 1
+    self.each_with_index do |num, idx|
+      hash *= (((19 * idx) + num) * 187)
+    end
+    hash
   end
 end
 
 class String
   def hash
+    hash = 1
+    alphabet = ("A".."z").to_a
+    arr = self.split("")
+    arr.each_with_index do |char, idx|
+      hash *= (((19 * idx) + alphabet.index(char)) * 187)
+    end
+    hash
   end
 end
 
@@ -16,6 +28,8 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash = 1
+    self.each { |k, v| hash *= (k.hash + v.hash) }
+    hash
   end
 end
