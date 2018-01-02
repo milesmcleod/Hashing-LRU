ef816a6110d7fcbae918258a5dfd568f10998e5b
Miles McLeod <milesmcleod123@gmail.com>
2018-01-02 17:18:07 -0300

    Complete palindrome exercise

--- firstname_lastname 5/lib/p07_practical.rb
+++ firstname_lastname 5/lib/p07_practical.rb
@@ -1,5 +1,18 @@
 require_relative 'p05_hash_map'
 
-def can_string_be_palindrome?(string) 
-  
+def can_string_be_palindrome?(string)
+  map = HashMap.new
+  single_counts = 0
+  string.split("").each do |char|
+    if map[char]
+      map[char] += 1
+    else
+      map[char] = 1
+    end
+  end
+  map.each do |k, v|
+    single_counts += 1 if v % 2 != 0
+  end
+  return false if single_counts > 1
+  true
 end
