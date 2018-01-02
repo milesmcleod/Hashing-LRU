100dd02caa47babaff030a673df9d409616d15b4
Miles McLeod <milesmcleod123@gmail.com>
2018-01-02 17:12:29 -0300

    Complete LRU Cache

--- firstname_lastname 5/lib/p06_lru_cache.rb
+++ firstname_lastname 5/lib/p06_lru_cache.rb
@@ -1,5 +1,6 @@
 require_relative 'p05_hash_map'
 require_relative 'p04_linked_list'
+require 'byebug'
 
 class LRUCache
   attr_reader :count
@@ -15,6 +16,12 @@ class LRUCache
   end
 
   def get(key)
+    if @map[key]
+      update_node!(@map[key])
+    else
+      calc!(key)
+    end
+    @map[key].val
   end
 
   def to_s
@@ -24,13 +31,20 @@ class LRUCache
   private
 
   def calc!(key)
-    # suggested helper method; insert an (un-cached) key
+    eject! if @map.count == @max
+    square = @prc.call(key)
+    @store.append(key, square)
+    @map[key] = @store.last
   end
 
   def update_node!(node)
-    # suggested helper method; move a node to the end of the list
+    node.remove
+    @store.append(node.key, node.val)
   end
 
   def eject!
+    node = @store.first
+    node.remove
+    @map.delete(node.key)
   end
 end
